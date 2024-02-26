9e7294e8208a86b5c7561c4df4b33fc5a5f7e0ab
Gustavo Van Erven <gvanerven@gmail.com>
2024-02-26 15:06:24 -0300

    inclusão do ENEM

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -38,6 +38,7 @@ default_sourcers = [
     {"descricao": "censoescolar", "url": "https://www.gov.br/inep/pt-br/acesso-a-informacao/dados-abertos/microdados/censo-escolar", "diretorio_zip": "../zips/censoescolar"},
     {"descricao": "encceja", "url": "https://www.gov.br/inep/pt-br/acesso-a-informacao/dados-abertos/microdados/encceja", "diretorio_zip": "../zips/encceja"},
     {"descricao": "saeb", "url": "https://www.gov.br/inep/pt-br/acesso-a-informacao/dados-abertos/microdados/saeb", "diretorio_zip": "../zips/saeb"}
+    {"descricao": "enem", "url": "https://www.gov.br/inep/pt-br/acesso-a-informacao/dados-abertos/microdados/enem", "diretorio_zip": "../zips/enem"}
 ]
 
 
